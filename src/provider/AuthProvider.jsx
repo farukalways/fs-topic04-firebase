0ee7beae9cc450b7af33758c2822c256107fcc19
farukalways <alishopon367@gmail.com>
2025-11-28 22:58:30 +0600

    login user

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,9 +1,11 @@
 import {
   createUserWithEmailAndPassword,
+  onAuthStateChanged,
   signInWithEmailAndPassword,
 } from "firebase/auth";
 import { AuthContext } from "../context/context";
 import { auth } from "./../firebase/firebase.init";
+import { useEffect } from "react";
 
 const AuthProvider = ({ children }) => {
   const userCreate = (email, password) => {
@@ -12,6 +14,18 @@ const AuthProvider = ({ children }) => {
   const signInUser = (email, password) => {
     return signInWithEmailAndPassword(auth, email, password);
   };
+
+  useEffect(() => {
+    const unsubscrib = onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        console.log(currentUser);
+      }
+    });
+
+    return () => {
+      unsubscrib();
+    };
+  }, []);
 
   const authInfo = { userCreate, signInUser };
 
